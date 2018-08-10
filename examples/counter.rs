50cbe2b67b7d8b3351f5f734c690186530752cec
phucvin <phucvin2@gmail.com>
2018-08-10 15:24:49 +0700

    refactor

--- examples/counter.rs
+++ examples/counter.rs
@@ -6,7 +6,7 @@ use tl_sync::*;
 
 const LOOPS: u32 = 5;
 
-fn heavy_computation() {
+fn heavy_computation() {/*
     let mut tmp = vec![];
     for _i in 1..10 {
         tmp.push(Tl::new(vec![1; 10_000]));
@@ -17,7 +17,7 @@ fn heavy_computation() {
     for it in tmp.iter() {
         it.sync(1, 0);
     }
-}
+*/}
 
 #[derive(PartialEq)]
 enum SyncStatus {
@@ -46,10 +46,12 @@ fn main() {
                 .name("main_ui".into())
                 .spawn(move || {
                     for i in 0..(LOOPS + 1) {
-                        let tmp = prepare_notify();
-                        tx.send(SyncStatus::JustPrepareNotify).unwrap();
-                        rx.recv().unwrap();
-                        notify(tmp);
+                        if i > 0 {
+                            let tmp = prepare_notify();
+                            tx.send(SyncStatus::JustPrepareNotify).unwrap();
+                            rx.recv().unwrap();
+                            notify(tmp);
+                        }
 
                         for _ in 0..3 {
                             heavy_computation();
@@ -90,18 +92,14 @@ fn main() {
         };
 
         for _ in 0..LOOPS {
-            assert!(ui_rx.recv().unwrap() == SyncStatus::JustPrepareNotify);
-            ui_rtx.send(()).unwrap();
-
             assert!(compute_rx.recv().unwrap() == SyncStatus::Idle);
             compute_rtx.send(()).unwrap();
             assert!(compute_rx.recv().unwrap() == SyncStatus::JustSync);
 
             ui_rtx.send(()).unwrap();
+            assert!(ui_rx.recv().unwrap() == SyncStatus::JustPrepareNotify);
+            ui_rtx.send(()).unwrap();
         }
-        
-        assert!(ui_rx.recv().unwrap() == SyncStatus::JustPrepareNotify);
-        ui_rtx.send(()).unwrap();
 
         ui_thread.join().unwrap();
         compute_thread.join().unwrap();
