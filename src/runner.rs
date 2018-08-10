3f6378cece12c1d545d7915b518084b981623f3c
phucvin <phucvin2@gmail.com>
2018-08-11 00:47:13 +0700

    setup return tick and stop functions

--- src/runner.rs
+++ src/runner.rs
@@ -9,6 +9,70 @@ enum SyncStatus {
     Quit,
 }
 
+pub fn setup<T: 'static + Send + Sync + Clone>(
+    root: T,
+    ui: fn(T),
+    compute: fn(T)
+) -> (Box<Fn()>, Box<Fn()>) {
+    init_dirties();
+
+    let (compute_tx, compute_rx) = mpsc::channel();
+    let compute_rtx: mpsc::Sender<bool>;
+
+    let _compute_thread = {
+        let root = root.clone();
+        let (tx, rx) = mpsc::channel();
+        compute_rtx = tx.clone();
+        let tx = compute_tx.clone();
+
+        thread::Builder::new()
+            .name("1_compute".into())
+            .spawn(move || {
+                loop {
+                    compute(root.clone());
+                    sync_from(2);
+
+                    tx.send(SyncStatus::Idle).unwrap();
+                    match rx.recv() {
+                        Ok(true) => (),
+                        _ => break,
+                    }
+                    sync_to(0);
+                    tx.send(SyncStatus::JustSync).unwrap();
+                }
+            }).unwrap()
+    };
+
+    let compute_rtx2 = compute_rtx.clone();
+
+    (
+        Box::new(move || {
+            let prepared = prepare_notify();
+            notify(prepared);
+            ui(root.clone());
+
+            match compute_rx.recv() {
+                Ok(SyncStatus::Idle) => (),
+                Ok(SyncStatus::Quit) => return,
+                _ => return,
+            }
+
+            compute_rtx.send(true).unwrap();
+            match compute_rx.recv() {
+                Ok(SyncStatus::JustSync) => (),
+                Ok(SyncStatus::Quit) => return,
+                _ => return,
+            }
+        }),
+        Box::new(move || {
+            compute_rtx2.send(false).unwrap();
+
+            prepare_notify();
+            drop_dirties();
+        })
+    )
+}
+
 pub fn run<T: 'static + Send + Sync + Clone>(
     root: T,
     ui: fn(T) -> bool,
