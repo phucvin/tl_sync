b4c1e1b0cadc4f253606cd65516090aa91b886ff
phucvin <phucvin2@gmail.com>
2018-08-11 13:54:30 +0700

    cleanup

--- src/runner.rs
+++ src/runner.rs
@@ -8,7 +8,6 @@ use std::time::{Duration, Instant};
 enum SyncStatus {
     Idle,
     JustSync,
-    Quit,
 }
 
 pub trait UiSetup {
@@ -94,7 +93,6 @@ pub fn setup<T: 'static + Send + Clone + UiSetup + ComputeSetup>(
         }
         match compute_rx.recv_timeout(compute_update_duration - ui_elapsed) {
             Ok(SyncStatus::Idle) => (),
-            Ok(SyncStatus::Quit) => return,
             Err(mpsc::RecvTimeoutError::Timeout) => {
                 // let total_elapsed = now.elapsed();
                 // let compute_elapsed = total_elapsed - ui_elapsed;
@@ -119,7 +117,6 @@ pub fn setup<T: 'static + Send + Clone + UiSetup + ComputeSetup>(
         // to avoid incomplete data sync when render UI
         match compute_rx.recv() {
             Ok(SyncStatus::JustSync) => (),
-            Ok(SyncStatus::Quit) => return,
             _ => return,
         }
         // let sync_elapsed = now.elapsed() - ui_elapsed - compute_elapsed;
@@ -137,82 +134,4 @@ pub fn setup<T: 'static + Send + Clone + UiSetup + ComputeSetup>(
     });
 
     (tick, stop)
-}
-
-pub fn run<T: 'static + Send + Sync + Clone>(root: T, ui: fn(T) -> bool, compute: fn(T) -> bool) {
-    init_dirties();
-    {
-        let (compute_tx, compute_rx) = mpsc::channel();
-        let compute_rtx: mpsc::Sender<()>;
-
-        let compute_thread = {
-            let root = root.clone();
-            let (tx, rx) = mpsc::channel();
-            compute_rtx = tx.clone();
-            let tx = compute_tx.clone();
-
-            thread::Builder::new()
-                .name("1_compute".into())
-                .spawn(move || loop {
-                    let ret = compute(root.clone());
-                    sync_from(2);
-
-                    tx.send(SyncStatus::Idle).unwrap();
-                    rx.recv().unwrap();
-                    sync_to(0);
-                    tx.send(SyncStatus::JustSync).unwrap();
-
-                    if ret == false {
-                        tx.send(SyncStatus::Quit).unwrap();
-                        break;
-                    }
-                }).unwrap()
-        };
-
-        loop {
-            let prepared = prepare_notify();
-            notify(prepared);
-            let ret = ui(root.clone());
-
-            match compute_rx.recv() {
-                Ok(SyncStatus::Idle) => (),
-                Ok(SyncStatus::Quit) => break,
-                _ => break,
-            }
-
-            compute_rtx.send(()).unwrap();
-            match compute_rx.recv() {
-                Ok(SyncStatus::JustSync) => (),
-                Ok(SyncStatus::Quit) => break,
-                _ => break,
-            }
-
-            if ret == false {
-                prepare_notify();
-                break;
-            }
-        }
-
-        compute_thread.join().unwrap();
-    }
-    drop_dirties();
-}
-
-pub fn run_single<T: 'static + Clone>(root: T, ui: fn(T) -> bool, compute: fn(T) -> bool) {
-    init_dirties();
-    {
-        loop {
-            notify(prepare_notify());
-            get_dirties().to_mut(0).clear();
-
-            let ui_ret = ui(root.clone());
-            let compute_ret = compute(root.clone());
-            sync_from(2);
-
-            if ui_ret == false || compute_ret == false {
-                break;
-            }
-        }
-    }
-    drop_dirties();
 }
