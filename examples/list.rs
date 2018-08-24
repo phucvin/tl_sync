58856bb73190b3a8295fcc01da05c562a62f202a
phucvin <phucvin2@gmail.com>
2018-08-25 00:25:37 +0700

    .

--- examples/list.rs
+++ examples/list.rs
@@ -6,11 +6,29 @@ use std::sync::Mutex;
 use std::time::Duration;
 use tl_sync::*;
 
+struct VerifyAction<T: Clone> {
+    trigger: Action<T>,
+    verified: Action<T>,
+}
+
+impl<T: Clone> VerifyAction<T> {
+    fn new() -> Self {
+        Self {
+            trigger: Action::new(),
+            verified: Action::new(),
+        }
+    }
+
+    fn transfer(&self) {
+        trigger.for_each(|it| self.verified.fire(it.clone()));
+    }
+}
+
 #[derive(Clone)]
 struct Root {
     money: Tl<usize>,
-    on_inc_money: Action<(usize, String)>,
-    on_dec_money: Action<(usize, String)>,
+    on_upgrade_item: VerifyAction<String>,
+    on_iap: Action<usize>,
     listeners: Wrc<Mutex<Vec<ListenerHandleRef>>>,
 }
 
@@ -18,8 +36,8 @@ impl Root {
     fn new() -> Self {
         Self {
             money: Tl::new(1000),
-            on_inc_money: Action::new(),
-            on_dec_money: Action::new(),
+            on_upgrade_item: VerifyAction::new(),
+            on_iap: Action::new(),
             listeners: Default::default(),
         }
     }
@@ -33,14 +51,38 @@ impl Root {
     }
 
     fn setup(&self) {
-        self.push(register_listener_2(&self.on_inc_money, &self.on_dec_money, {
+        self.push(register_listener_1(&self.on_upgrade_item.trigger, {
             let this = self.clone_weak();
             move || {
-                let mut total_inc = 0;
-                let mut total_dec = 0;
-                
-                this.on_inc_money.for_each(|it| total_inc += it.0);
-                this.on_dec_money.for_each(|it| total_dec -= it.0);
+                let mut required_money = 0;
+
+                for item_id in this.on_upgrade_item.trigger {
+                    let item = this.item_map.get(item_id).unwrap();
+
+                    required_money += *item.value;
+                }
+
+                if *this.money >= required_money {
+                    this.on_upgrade_item.transfer();
+                } else {
+                    // TODO Show/Toast error to UI
+                }
+            }
+        }));
+
+        self.push(register_listener_2(&self.on_upgrade_item.verified, &self.on_iap, {
+            let this = self.clone_weak();
+            move || {
+                let mut inc = 0;
+                let mut dec = 0;
+
+                this.on_iap.for_each(|it| inc += it);
+
+                this.on_upgrade_item.verified.for_each(|it| {
+                    let item = this.item_map.get(it);
+
+                    dec += *item.value;
+                });
 
                 assert!(*this.money + total_inc <= 2_000_000_000, "overflow");
                 assert!(total_dec <= *this.money + total_inc, "invalid money inc/dec");
@@ -59,19 +101,18 @@ impl Root {
 struct Item {
     id: String,
     value: Tl<isize>,
-    on_upgrade: Action<()>,
+    // Demo only, action should be at top level
+    on_use: Action<isize>,
     listeners: Wrc<Mutex<Vec<ListenerHandleRef>>>,
-    root: Wrc<Root>,
 }
 
 impl Item {
-    fn new(id: String, value: isize, root: Root) -> Self {
+    fn new(id: String, value: isize) -> Self {
         Self {
             id,
             value: Tl::new(value),
-            on_upgrade: Action::new(),
+            on_use: Action::new(),
             listeners: Default::default(),
-            root: Wrc::new(root),
         }
     }
 
@@ -79,21 +120,48 @@ impl Item {
         let mut ret = self.clone();
 
         ret.listeners.be_weak();
-        ret.root.be_weak();
 
         ret
     }
 
     fn setup(&self) {
-        self.defer(register_listener_1(&self.on_upgrade, {
+        let root = self.root.make_strong();
+
+        self.defer(register_listener_1(&self.on_use.trigger, {
+            let this = self.clone_weak();
+            move || {
+                let mut required_value = 0;
+
+                for it in this.on_use.trigger {
+                    required_value += it;
+                }
+
+                if *this.value > required_value {
+                    this.on_use.transfer();
+                } else {
+                    // TODO Show/Toast error to UI
+                }
+            }
+        }));
+
+        self.defer(register_listener_1(&root.on_upgrade_item, &root.on_use_item, {
             let this = self.clone_weak();
             move || {
                 let root = this.root.make_strong();
+                let mut inc = 0;
+                let mut dec = 0;
 
-                if *root.money > 100 {
-                    *this.value.to_mut() += 1;
-                    root.on_dec_money.fire((100, "upgrade item"));
-                }
+                root.on_upgrade_item.verified.for_each(|it| if it == this.id {
+                    inc += 10;
+                });
+
+                root.on_use_item.verified.for_each(|it| if it.0 == this.id {
+                    dec += it.1;
+                });
+
+                if inc == 0 && dec == 0 { return; }
+                assert!(*this.value + inc - dec >= 0);
+                *this.value.to_mut() = *this.value + inc - dec;
             }
         }));
     }
